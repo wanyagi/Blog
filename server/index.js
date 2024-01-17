864363102fdce9b74738ca624a2586abe09ad6f7
daniel <danielwanyagi@outlook.com>
2024-01-17 08:05:22 +0100

    ...

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 
 const port = process.env.PORT ;
 
-app.use(cors({ origin: process.env.URL, credentials: true, })); 
+app.use(cors({ origin: [process.env.URL, process.env.URL.replace(/\/$/, '')], credentials: true, })); 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser()); 
