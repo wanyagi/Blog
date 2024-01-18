92ccf3dcaa3963db740f0f6f8148b652f5ab6f32
daniel <danielwanyagi@outlook.com>
2024-01-18 16:58:03 +0100

    18. Changed my DB credentials.

--- server/database.js
+++ server/database.js
@@ -4,11 +4,7 @@ require('dotenv').config();
 
 const pool = new Pool({
     client_encoding: process.env.CODE,
-    DATABASE_USER: process.env.USER, 
-    DATABASE_PASSWORD: process.env.PASSWORD,
-    DATABASE_HOST: process.env.HOST, 
-    DATABASE_PORT: process.env.PORT,
-    DATABASE: process.env.DATABASE
+    DATABASE_URL: process.env.DATABASE_URL_RENDER, 
 }); 
 
 module.exports = pool; 
