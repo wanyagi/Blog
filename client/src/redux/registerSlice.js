e48a5cd3d0496fbef1a64480ba78a002307a487f
daniel <danielwanyagi@outlook.com>
2024-01-18 07:46:47 +0100

    18. ...

--- client/src/redux/registerSlice.js
+++ client/src/redux/registerSlice.js
@@ -11,7 +11,7 @@ export const registerUser = createAsyncThunk("user/registerUser", async ({name, 
       }); 
 
       if (!response.ok) {
-        throw new Error(error.message);
+        throw new Error("tu es ici registerslice");
       }; 
 
       const responseData = await response.json(); 
