cc14b0e1339906741a89097dae3c3aa3b778ac60
daniel <danielwanyagi@outlook.com>
2024-01-19 09:29:03 +0100

    19.5

--- server/database.js
+++ server/database.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 const pool = new Pool({
     client_encoding: process.env.CODE,
-    DB_URL: process.env.EXTERNAL_DB_RENDER_URL,
+    connectionString: process.env.EXTERNAL_DB_RENDER_URL,
 }); 
 
 module.exports = pool; 
