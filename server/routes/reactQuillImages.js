87fdb2648f0af11989643ae498e14440ef55eef5
daniel <danielwanyagi@outlook.com>
2024-01-27 16:49:51 +0100

    25.6. ...

--- server/routes/reactQuillImages.js
+++ server/routes/reactQuillImages.js
@@ -5,7 +5,7 @@ const multer  = require('multer');
 
 const imageStorage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/uploads/images');
+        cb(null, '/uploads');
     },
     filename: function (req, file, cb) {
         cb(null, `${Date.now()}_${file.originalname}`);
